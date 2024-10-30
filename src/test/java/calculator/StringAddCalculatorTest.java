be7925005824390289348bf01659b747fc72e13b
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-10-30 21:39:03 +0900

    tests: 문자열 계산기 기능 단위 테스트 구현

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+
+public class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @DisplayName("빈 문자열 또는 Null을 입력 하는 경우 0을 반환한다")
+    @NullAndEmptySource
+    void test_빈문자열_null_0반환(String given) {
+        StringNumber stringNumber = StringNumber.from(given);
+
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(stringNumber);
+
+        assertThat(stringAddCalculator.calculate()).isZero();
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 하나만 입력 하면 해당 숫자를 반환한다")
+    @CsvSource(value = {"1, 1", "10, 10"})
+    void test_숫자_한개_반환(String given, int expected) {
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(StringNumber.from(given));
+        assertThat(stringAddCalculator.calculate()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("숫자 사이에 구분자(',')를 포함 하면 숫자의 합을 반환한다")
+    void test_숫자_두개_합_구분자_한개() {
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(StringNumber.from("1,2"), ",");
+        assertThat(stringAddCalculator.calculate()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("숫자 사이에 구분자(',' 또는 '|')를 포함 하면 숫자의 합을 반환한다")
+    void test_숫자_세개_합_구분자_두개() {
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(StringNumber.from("1,2:3"), ",|:");
+        assertThat(stringAddCalculator.calculate()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("숫자 사이에 커스텀 구분자를 포함 하면 숫자의 합을 반환한다")
+    void test_숫자_세개_합_구분자_커스텀() {
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(StringNumber.from("//;\n1;2;3"), "//(.)\n(.*)");
+        assertThat(stringAddCalculator.calculate()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("음수를 입력한 경우 'RuntimeException'을 반환한다")
+    void test_음수_입력_에러() {
+        StringAddCalculator stringAddCalculator = StringAddCalculator.from(StringNumber.from("-1,2,3"), ",");
+        assertThatThrownBy(() -> {
+            stringAddCalculator.calculate();
+        }).isInstanceOf(RuntimeException.class).hasMessageContaining("음수는 입력 할 수 없습니다");
+
+    }
+}
