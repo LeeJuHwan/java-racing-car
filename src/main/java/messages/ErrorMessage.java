13ee47ba628b772f2255183aafaba1914499c803
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-16 16:52:55 +0900

    chore: 어플리케이션 메세지 관리

---
+++ src/main/java/messages/ErrorMessage.java
@@ -0,0 +1,9 @@
+package messages;
+
+public class ErrorMessage {
+
+    public static final String invalidCarName = "자동차 이름은 비어있거나 5글자를 초과 할 수 없습니다.";
+    public static final String invalidCarMoveThresold = "자동차를 전진 시키기 위해 0과 9사이의 숫자만 입력 해야합니다.";
+    public static final String CarsNotFound = "자동차 목록을 찾을 수 없습니다.";
+
+}
